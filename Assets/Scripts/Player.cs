eaf552f45853755ce13339974fe344634238a056
Drkr <drkrthanlife@gmail.com>
2017-12-02 12:44:26 +0800

    basic attraction script written

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public static Player INSTANCE = null;
+
+	// Use this for initialization
+	void Start ()
+    {
+        if (INSTANCE == null)
+        {
+            INSTANCE = this;
+        }
+        else
+        {
+            DestroyImmediate(gameObject);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/TreasureMagnet.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreasureMagnet : MonoBehaviour
+{
+
+    //maximum radius of attraction
+    public const float THRESHOLD = 10f;
+
+    public const float magnitude = 10f;
+    Rigidbody2D rb;
+    Vector3 dir;
+    float dist;
+
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //distance between player and this
+        dist = Vector3.Distance(Player.INSTANCE.transform.position, transform.position);
+
+        //if player is within threshold
+        if (dist < THRESHOLD)
+        {
+            //gets direction vector towards player
+            dir = Vector3.Normalize(Player.INSTANCE.transform.position - transform.position);
+
+
+            //force towards player is mass times magnitude divided by distance
+            //further away the player is the weaker the force
+            rb.AddForce(dir * magnitude * rb.mass / dist);
+        }
+        else
+        {
+
+        }
+	}
+}
