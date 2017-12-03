946a937cf715e8f3e985e06cb4c1a4e748618cc2
Hazeem Adly Ariff <hazeemadlyariff@gmail.com>
2017-12-03 14:29:20 +0800

    a few goodies

---
+++ Assets/Scripts/Spikes.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikes : MonoBehaviour {
+
+    Rigidbody2D rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = Player.INSTANCE.gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        print(other.gameObject);
+
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Player.health = 0;
+        }
+    }
+}
